d6b3296c6600dc85abe6788b77db0b2e5fb53c75
mljustinli <ml.justin.li@gmail.com>
2017-09-30 14:03:46 -0400

    Modify playermovement script

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -16,52 +16,91 @@ public class PlayerMovementController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		int horizontal = 0;
-		int vertical = 0;
 
-		if (Input.GetKey(KeyCode.A))
-		{
-			horizontal--;
-		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			horizontal++;
-		}
-		rb.velocity = new Vector3(horizontal * movespeed, rb.velocity.y, 0);
+		//leftcheck, rightcheck, and isgrounded mean they're in the collider/intersecting
 
-//		if (Input.GetKeyUp(KeyCode.A)) {
-//			LeftCheck = false;
+		if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+			rb.velocity = new Vector3 (rb.velocity.x, jumpspeed, 0);
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			rb.velocity = new Vector2 (-movespeed, rb.velocity.y);
+		}
+		if (Input.GetKeyUp(KeyCode.A)) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			rb.velocity = new Vector2 (movespeed, rb.velocity.y);
+		}
+		if (Input.GetKeyUp(KeyCode.D)) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (LeftCheck && Input.GetKey(KeyCode.A)) {
+			rb.velocity = new Vector3 (rb.velocity.x, -0.6f, 0);
+		} else if (RightCheck && Input.GetKey(KeyCode.D)) {
+			rb.velocity = new Vector3 (rb.velocity.x, -0.6f, 0);
+		}
+
+		if (LeftCheck && Input.GetKeyDown(KeyCode.Space)) {
+			rb.velocity = new Vector3 (3.0f, jumpspeed, 0);
+		} else if (RightCheck && Input.GetKeyDown(KeyCode.Space)) {
+			rb.velocity = new Vector3 (-3.0f, jumpspeed, 0);
+		}
+
+
+
+
+
+
+
+
+//		int horizontal = 0;
+//		int vertical = 0;
+//
+//		if (Input.GetKey(KeyCode.A))
+//		{
+//			horizontal--;
 //		}
-//		if (Input.GetKeyUp(KeyCode.D)) {
-//			RightCheck = false;
+//		if (Input.GetKey(KeyCode.D))
+//		{
+//			horizontal++;
 //		}
-
-//		if (Input.GetKeyDown(KeyCode.Space) && (isGrounded) && !LeftCheck && !RightCheck)
-		if (Input.GetKeyDown(KeyCode.Space) && (isGrounded))
-		{
-			vertical++;
-			rb.velocity = new Vector3(rb.velocity.x, vertical * jumpspeed, 0);
-			isGrounded = false;
-		}
-
-		if (LeftCheck && Input.GetKey(KeyCode.A))
-		{
-			rb.velocity = new Vector3(rb.velocity.x, -0.6f, 0);
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				rb.velocity = new Vector3(3, jumpspeed, 0);
-				LeftCheck = false;
-			}
-		}
-		if (RightCheck && Input.GetKey(KeyCode.D))
-		{
-			rb.velocity = new Vector3(rb.velocity.x, -0.6f, 0);
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				rb.velocity = new Vector3(-3, jumpspeed, 0);
-				RightCheck = false;
-			}
-		}
+//		rb.velocity = new Vector3(horizontal * movespeed, rb.velocity.y, 0);
+//
+////		if (Input.GetKeyUp(KeyCode.A)) {
+////			LeftCheck = false;
+////		}
+////		if (Input.GetKeyUp(KeyCode.D)) {
+////			RightCheck = false;
+////		}
+//
+////		if (Input.GetKeyDown(KeyCode.Space) && (isGrounded) && !LeftCheck && !RightCheck)
+//		if (Input.GetKeyDown(KeyCode.Space) && (isGrounded))
+//		{
+//			vertical++;
+//			rb.velocity = new Vector3(rb.velocity.x, vertical * jumpspeed, 0);
+//			isGrounded = false;
+//		}
+//
+//		if (LeftCheck && Input.GetKey(KeyCode.A))
+//		{
+//			rb.velocity = new Vector3(rb.velocity.x, -0.6f, 0);
+//			if (Input.GetKeyDown(KeyCode.Space))
+//			{
+//				rb.velocity = new Vector3(3, jumpspeed, 0);
+//				LeftCheck = false;
+//			}
+//		}
+//		if (RightCheck && Input.GetKey(KeyCode.D))
+//		{
+//			rb.velocity = new Vector3(rb.velocity.x, -0.6f, 0);
+//			if (Input.GetKeyDown(KeyCode.Space))
+//			{
+//				rb.velocity = new Vector3(-3, jumpspeed, 0);
+//				RightCheck = false;
+//			}
+//		}
 
 		Camera.main.transform.position = transform.position + new Vector3(0, 0, -10);
 	}
